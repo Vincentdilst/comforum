edf31393ec6a84e39da0f375c7970a94eec10d2f
Rik Kampman <80103@b5-400b-28.ict.lab.locals>
2017-11-16 09:54:29 +0100

    Eerste backup vanaf rik 16nov2017

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+// connect.php
+include 'config.php';
+// kan vervangen worder door.
+$server = 'localhost';
+$username   = 'usernamehere';
+$password   = 'passwordhere';
+$database   = 'databasenamehere';
+
+if (!mysql_connect($db_hostname, $db_username, $db_password))
+{
+	exit('Error: kon niet met de database verbinden.');
+}
+if (!mysql_select_db($db_database))
+{
+	exit('Error: kan de database niet vinden');
+}
